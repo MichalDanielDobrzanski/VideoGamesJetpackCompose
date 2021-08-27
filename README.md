47ee370dd9ce9fb9188af933ce4dc0025cba70a6
Michal Dobrzanski <xxasm@eg.dk>
2021-08-27 13:58:07 +0200

    Added dark mode preview

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 1. https://developer.android.com/jetpack/compose/interop/adding
 2. https://developer.android.com/jetpack/compose/tutorial
-3. Have Java 11 installed (Can be )
+3. Have Java 11 installed (Can be embedded from AS)
+4. Material Design is built around three pillars: Color, Typography, Shape
--- app/src/main/java/com/dobi/videogames/presentation/main/MainActivity.kt
+++ app/src/main/java/com/dobi/videogames/presentation/main/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.dobi.videogames.presentation.main
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dobi.videogames.R
+import com.dobi.videogames.presentation.main.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -15,18 +34,68 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Mike")
+            ComposeActivityTheme {
+                MessageCard(Message("Mike", "Compose is great"))
+            }
         }
     }
 
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello $name")
+    fun MessageCard(message: Message) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Android logo",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+
+            }
+        }
     }
 
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(name = "Toolset")
+        ComposeActivityTheme {
+            MessageCard(message = Message("Toolset Preview", "I can render it here"))
+        }
     }
+
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode preview"
+    )
+    @Composable
+    fun PreviewMessageCardInDarkMode() {
+        ComposeActivityTheme {
+            MessageCard(message = Message("Toolset Preview", "I can render it here"))
+        }
+    }
+
+    data class Message(
+        val author: String,
+        val body: String
+    )
 }
